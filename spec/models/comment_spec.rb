912f4603dfc0ba875a4ce76657126f508478ee1e
Buck Rivard <buckrivard@gmail.com>
2017-03-15 14:20:11 -0700

    fixed failing spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
- 	let(:topic) { Topic.create!(name: name, description: description) }
- 	let(:post) { topic.posts.create!(title: title, body: body) }
+ 	let(:topic) { Topic.create!(name: "topic name", description: "topic description") }
+ 	let(:post) { topic.posts.create!(title: 'post title', body: 'extra long post body') }
 	let(:comment) { Comment.create!(body: 'Comment Body', post: post) }
 
 	describe "attributes" do
