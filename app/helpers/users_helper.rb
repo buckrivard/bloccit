73c8edcd6607541dc7de96e2c87ddab07df4d769
Buck Rivard <buckrivard@gmail.com>
2017-03-22 09:02:24 -0700

    factories and helper created

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,17 @@
 module UsersHelper
+	def has_posts(user)
+		if user.posts.count == 0
+			"#{user.name} hasn't posted anything just yet."
+		else
+			render @user.posts
+		end
+	end
+
+	def has_comments(user)
+		if user.comments.count == 0
+			"#{user.name} hasn't commented on anything yet"
+		else
+			render @user.comments
+		end
+	end
 end
