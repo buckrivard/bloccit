59fc37a722b186472ff2253cbd522716d2972287
Buck Rivard <buckrivard@gmail.com>
2017-03-22 12:13:01 -0700

    comments not displaying

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
 	def has_posts(user)
 		if user.posts.count == 0
-			"#{user.name} hasn't posted anything just yet."
+			"#{@user.name} hasn't posted anything just yet."
 		else
 			render @user.posts
 		end
@@ -9,7 +9,7 @@ module UsersHelper
 
 	def has_comments(user)
 		if user.comments.count == 0
-			"#{user.name} hasn't commented on anything yet"
+			"#{@user.name} hasn't commented on anything yet"
 		else
 			render @user.comments
 		end
